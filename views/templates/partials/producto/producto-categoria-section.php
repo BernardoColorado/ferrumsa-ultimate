2265e2261fb8be3df2236bef8f5b99f34e6b3262
BernardoColorado <bernardocoloradodubois.hw@gmail.com>
2021-01-06 00:14:05 -0600

    ajustando

--- views/templates/partials/producto/producto-categoria-section.php
+++ views/templates/partials/producto/producto-categoria-section.php
@@ -2,19 +2,19 @@
 
   <div class="container py-5">
     <div class="row">
-      <div class="col-lg-8 mx-auto">
+      <div class="col-lg-7 mx-auto">
         <div class="card rounded-0 border-0">
           <div class="row no-gutters">
             <div class="col-lg-12">
               <div class="card-body bg-danger">
-                <h2 class="card-title text-light text-uppercase text-center">productos</h2>
+                <h3 class="card-title text-light text-uppercase text-center">productos</h3>
               </div>
             </div>
           </div>
           <div class="row no-gutters">
             <div class="col-lg-12">
               <div class="card-body bg-warning">
-                <h3 class="card-title text-light text-uppercase text-center">{{ categoria.nombre }}</h3>
+                <h4 class="card-title text-light text-uppercase text-center">{{ categoria.nombre }}</h4>
               </div>
             </div>
           </div>
