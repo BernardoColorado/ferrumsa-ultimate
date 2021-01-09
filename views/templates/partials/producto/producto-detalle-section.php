6d62228dd02b2bd2e9183f97edd61f53c359c753
BernardoColorado <bernardocoloradodubois.hw@gmail.com>
2021-01-08 22:42:06 -0600

    agregando acabados de productos

--- views/templates/partials/producto/producto-detalle-section.php
+++ views/templates/partials/producto/producto-detalle-section.php
@@ -21,6 +21,7 @@
   <div class="container py-5">
 
     <div class="row no-gutters">
+
       <div class="col-lg-3">
         <div class="card border-0 rounded-0 h-100">
           <img src="{{ base_url() }}/img/producto/{{producto.getImg()}}" alt="" class="card-img border-0 rounded-0">
@@ -29,6 +30,7 @@
           </div>
         </div>
       </div>
+
       <div class="col-lg-9">
         <div class="card card-body h-100 border-0 rounded-0 bg-light">
           <h3 class="card-title text-danger text-center text-uppercase">{{ producto.getNombre() }}</h3>
@@ -36,6 +38,13 @@
             <p class="text-dark">{{ descripcion.getTexto() }}</p>
           {% endfor %}
         </div>
+      </div>
+
+    </div>
+
+    <div class="row no-gutters">
+      <div class="col-lg-2">
+        <img class="img-fluid" src="{{ base_url() }}/" alt="">
       </div>
     </div>
 
