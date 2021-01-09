322a8caf01a155dbe10508160be71622676e526b
BernardoColorado <bernardocoloradodubois.hw@gmail.com>
2021-01-09 13:58:11 -0600

    ajustando margenes

--- views/templates/partials/general/footer-section.php
+++ views/templates/partials/general/footer-section.php
@@ -1,11 +1,12 @@
   <section id="footer" class="text-center text-light sticky-bottom">
     <div class="container">
       <div class="row my-3">
-        <div class="col-lg-4 mx-auto">
+        <div class="col-lg-3 mx-auto">
           <div class="social d-flex justify-content-center">
-            <a class="mx-5 text-white" href=""><h1><i class="fab fa-whatsapp"></i></h1></a>
-            <a class="mx-5 text-white" href=""><h1><i class="fab fa-instagram"></i></h1></a>
-            <a class="mx-5 text-white" href="https://www.facebook.com/contactoferrumsa"><h1><i class="fab fa-facebook"></i></h1></a>
+            <a class="mx-5 text-white" href="{{socialNetworks.whatsapp}}"><h1><i class="fab fa-whatsapp"></i></h1></a>
+            <a class="mx-5 text-white" href="{{socialNetworks.instagram}}"><h1><i class="fab fa-instagram"></i></h1></a>
+            <a class="mx-5 text-white" href="{{socialNetworks.facebook}}"><h1><i class="fab fa-facebook"></i></h1></a>
+            <a class="mx-5 text-white" href="{{socialNetworks.linkedin}}"><h1><i class="fab fa-linkedin"></i></h1></a>
           </div>
         </div>
       </div>
