8c46cfeb29efc4cd30a3fc778e9116028e8eab4f
BernardoColorado <bernardocoloradodubois.hw@gmail.com>
2021-01-09 18:25:33 -0600

    footer expandido

--- views/templates/partials/general/footer-section.php
+++ views/templates/partials/general/footer-section.php
@@ -3,15 +3,15 @@
       <div class="row my-3">
         <div class="col-lg-3 mx-auto">
           <div class="social d-flex justify-content-center">
-            <a class="mx-5 text-white" href="{{socialNetworks.whatsapp}}"><h1><i class="fab fa-whatsapp"></i></h1></a>
-            <a class="mx-5 text-white" href="{{socialNetworks.instagram}}"><h1><i class="fab fa-instagram"></i></h1></a>
-            <a class="mx-5 text-white" href="{{socialNetworks.facebook}}"><h1><i class="fab fa-facebook"></i></h1></a>
-            <a class="mx-5 text-white" href="{{socialNetworks.linkedin}}"><h1><i class="fab fa-linkedin"></i></h1></a>
+            <a class="mx-5 text-white" href="{{socialNetworks.whatsapp}}"><h2><i class="fab fa-whatsapp"></i></h2></a>
+            <a class="mx-5 text-white" href="{{socialNetworks.instagram}}"><h2><i class="fab fa-instagram"></i></h2></a>
+            <a class="mx-5 text-white" href="{{socialNetworks.facebook}}"><h2><i class="fab fa-facebook"></i></h2></a>
+            <a class="mx-5 text-white" href="{{socialNetworks.linkedin}}"><h2><i class="fab fa-linkedin"></i></h2></a>
           </div>
         </div>
       </div>
       <div class="row my-3">
-        <div class="col-lg-12">
+        <div class="col-lg-22">
           <small>Derechos Reservados &copy; Ferrumsa 2020</small>  
         </div>
       </div>
