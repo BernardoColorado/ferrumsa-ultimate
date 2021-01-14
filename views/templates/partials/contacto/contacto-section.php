fa7f6fd1a7593ec444eef06cbc56dd0975624969
BernardoColorado <bernardocoloradodubois.hw@gmail.com>
2021-01-14 00:39:11 -0600

    ajustes finales en google

--- views/templates/partials/contacto/contacto-section.php
+++ views/templates/partials/contacto/contacto-section.php
@@ -20,7 +20,7 @@
           <div class="row">
             <div class="col">
               <a href="{{socialNetworks.whatsapp}}">
-                <h5 class="text-center"><i class="fas fa-phone fa-2x mt-4 text-warning"></i></h5>
+                <h5 class="text-center"><i class="fas fa-phone fa-2x mt-4 text-danger"></i></h5>
                 <h6 class="text-center text-light text-uppercase">teléfono</h6>
                 <h5 class="text-center text-light font-weight-bold">81-17-90-67-96</h5>
               </a>
@@ -29,7 +29,7 @@
 
           <div class="row">
             <div class="col">
-              <h5 class="text-center"><i class="fas fa-envelope fa-2x mt-4 text-warning"></i></h5>
+              <h5 class="text-center"><i class="fas fa-envelope fa-2x mt-4 text-danger"></i></h5>
               <h6 class="text-center text-light text-uppercase">mail</h6>
               <h5 class="text-center text-light font-weight-bold">contacto@ferrumsa.com</h5>
             </div>
@@ -38,7 +38,7 @@
           <div class="row">
             <div class="col">
               <a href="{{socialNetworks.maps}}">
-                <h5 class="text-center"><i class="fas fa-map-marker-alt fa-2x mt-4 text-warning"></i></h5>
+                <h5 class="text-center"><i class="fas fa-map-marker-alt fa-2x mt-4 text-danger"></i></h5>
                 <h6 class="text-center text-light text-uppercase">ubicación</h6>
                 <h5 class="text-center text-light font-weight-bold">Antonio E. Marcos Lote 5, Colonia Cosmópolis, Apodaca Nuevo León, CP:66612</h5>
               </a>
